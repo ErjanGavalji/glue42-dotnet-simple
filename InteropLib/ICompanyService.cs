60f06f506e083dd91f58cb8ce423ae2ec3dd9910
Erjan Gavalji <erjan.gavalji@gmail.com>
2019-05-17 20:00:32 +0300

    Fix the naming

--- InteropLib/ICompanyService.cs
+++ InteropLib/ICompanyService.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace InteropLib
 {
-    [ServiceContract(MethodNamespace = "FidelityDemo.")]
+    [ServiceContract(MethodNamespace = "DotNetDemo.")]
     public interface ICompanyService : IDisposable
     {
         [ServiceOperation(AsyncIfPossible = true, ExceptionSafe = true)]
