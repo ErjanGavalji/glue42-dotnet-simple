c6b18ecb2a82d5a51b03f68f0ff8d6845dfd4fc8
Erjan Gavalji <erjan.gavalji@gmail.com>
2019-05-17 19:38:21 +0300

    Make the WinForms app interop properly

---
+++ CompanyManager_WF/CompanyService.cs
@@ -0,0 +1,26 @@
+﻿using InteropLib;
+using System;
+using System.Windows.Forms;
+
+namespace CompanyManager_WF
+{
+    internal class CompanyService : ICompanyService
+    {
+        private Action<string> handleCompanyCode;
+        public CompanyService(Action<string> handleCompanyCode)
+        {
+            this.handleCompanyCode = handleCompanyCode;
+        }
+
+        public void Dispose()
+        {
+            this.handleCompanyCode = null;
+        }
+
+        public bool ShowCompany(string code)
+        {
+            this.handleCompanyCode(code);
+            return true;
+        }
+    }
+}
